8795ec52d1e2195ec5de6e7d9bea9b72d0d3e3d6
Goldielockz30 <ahimbajohnson@hotmail.co.uk>
2023-02-10 12:32:56 +0000

    README

--- provision.sh
+++ provision.sh
@@ -5,31 +5,29 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-# Install nginx
+# Install web server nginx
 sudo apt-get install nginx -y
 
 
-# Enable or Start Nginx
+# Start and Enable Nginx
+sudo systemctl start nginx
 sudo systemctl enable nginx -y
 
-# This will install a particular version
+# This will install a particular version of python
+sudo apt-get install python -y
 sudo apt-get install python-software-properties 
 
 # Overwrite nodejs to particular version
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -  
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -  
 
-# Install Node.js version 6.x
+# Install Node.js 
 sudo apt-get install nodejs -y
 
 # Install pm2
 sudo npm install pm2 -g 
 
-# Install pm2
-npm install
+# Install Process Manager pm2
+sudo npm install
 
-# cd into app folder
-cd app
 
-# Run the node app
-node app.js
 
